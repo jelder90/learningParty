d1bf641526cdc1ea12a944c109d42b9b1463fe85
at0mbxmb <at0mbxmb@gmail.com>
2013-01-03 13:12:57 -0800

    added new hand class for hand of cards

--- BlackJackGame/src/Hand.java
+++ BlackJackGame/src/Hand.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
+
 /**
  * Created with IntelliJ IDEA.
  * User: michelle
@@ -5,5 +8,13 @@
  * Time: 10:02 AM
  * To change this template use File | Settings | File Templates.
  */
-public class Hand {
+public class Hand<Integer> extends ArrayList {
+
+    public Hand(int cap) {
+        Hand<Integer> hand = new Hand<Integer>(cap);
+    }
+
+    public void addCardToHand (Integer cardToAdd) {
+        this.addCardToHand(cardToAdd);
+    }
 }
